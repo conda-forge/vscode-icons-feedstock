ec4172ce92acf0b884af5fba3f0d8bbc7d11f680
Jan Tilly <jantilly@gmail.com>
2021-03-03 20:28:22 +0100

    Actually ship the extension files.
    Also pin code-server at >=3.6.0, which is when we began pinning nodejs
    at 12.*.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 EXTENSION_PATH="./vscode-icons.vsix"
-
+mkdir -p $PREFIX/etc/share/code-server/extensions
 npm install
 npm run build
 npx vsce package --out ${EXTENSION_PATH}
-code-server --install-extension ${EXTENSION_PATH}
+code-server --install-extension ${EXTENSION_PATH} --extensions-dir $PREFIX/share/code-server/extensions
