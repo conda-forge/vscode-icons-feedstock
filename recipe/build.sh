c8ccab1d3cbcb83a31250ff341a4f5b6fa63222a
Uwe Korn <uwelk@xhochy.com>
2023-09-29 12:24:56 +0000

    Fix recipe on linux-64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+
+set -euxo pipefail
+
 EXTENSION_PATH="./vscode-icons.vsix"
+
+rm $PREFIX/bin/node
+ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
+
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+    export npm_config_arch="arm64"
+fi
+
+# Don't use pre-built gyp packages
+export npm_config_build_from_source=true
+export NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
 npm install
 npm run build
