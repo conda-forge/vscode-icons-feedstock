d4d2dbf17f937b256e550332ae43091fda273a8e
Uwe Korn <uwelk@xhochy.com>
2023-09-29 12:33:31 +0000

    git commit

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,6 +15,13 @@ fi
 export npm_config_build_from_source=true
 export NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
+# Later install steps need a valid git repository
+git init .
+git config user.email "you@example.com"
+git config user.name "Your Name"
+git add .
+git commit -m "Initial commit" --no-verify --no-gpg-sign
+
 npm install
 npm run build
 npx vsce package --out ${EXTENSION_PATH}
